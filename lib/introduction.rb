5f8fdc80b4412f4289146c53332427a7451f0758
mohamad farahmandifard <ticticb80om@gmail.com>
2019-08-19 01:51:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,16 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language (name,language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
+
+def introduction_with_language_optional (name,language="ruby")
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
+
+
+
 
 
 
